0be2816cfbfe3e0aaf105dcbe7a18ac4bd2bb640
maximivanov1711 <maxim.ivanov1711@gmail.com>
2023-06-12 10:35:24 +0400

    Refactored file structure and added tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import { getTodosRouter } from './routes/todos'
+import { app } from './app'
 
-const app = express()
-const jsonBodyMiddleware = express.json()
-
-app.use(jsonBodyMiddleware)
-
-
-const mongodb_uri = 'mongodb+srv://maximivanov1711:iamaxim2004@cluster0.dpoadpm.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongodb_uri)
-
-app.use('/todos', getTodosRouter())
-
-const port = 3000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+const port = process.env.PORT || 5000
+export const server = app.listen(port, () => {
+  console.log(`Todo-List server listening on port ${port}`)
 })
